858877725f2dc3d8475b3314921a793177fc2803
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2016-12-03 17:58:45 +0200

    Modify navbar to display correct login link. Add confirmable to devise. Add devise for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  mount Sidekiq::Web => "/sidekiq" # monitoring console
+  devise_for :users
+  mount Sidekiq::Web => '/sidekiq' # monitoring console
 
-  root "home#index"
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
