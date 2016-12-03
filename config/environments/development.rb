fe2f894df255a1ebf6a02ff2c8e0ea45f2138719
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2016-12-03 14:51:13 +0200

    Add template for sending emails using mailgun to development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
+  # Settings specified here will take precedence
+  # over those in config/application.rb.
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -30,10 +30,20 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Ensure mailer works in development.
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
-  config.action_mailer.asset_host = "http://localhost:3000"
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.asset_host = 'http://localhost:3000'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    authentication: :plain,
+    address: 'smtp.mailgun.org',
+    port: 587,
+    domain: 'MYDOMAIN.mailgun.org',
+    user_name: 'postmaster@MYDOMAIN.mailgun.org',
+    password: 'MYPASSWORD'
+  }
 
   config.action_mailer.perform_caching = false
 
