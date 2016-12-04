3a850578a85374faffdc671ea5554d62b4c918c9
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2016-12-04 16:40:08 +0200

    Add admin role to db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :invitations
   resources :profiles
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -9,57 +9,76 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20161204003039) do
+
+ActiveRecord::Schema.define(version: 20161204143813) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'invitations', force: :cascade do |t|
-    t.integer  'user_id'
-    t.string   'invitee_name'
-    t.string   'invitee_email'
-    t.string   'invitee_title'
-    t.string   'invitee_company'
-    t.string   'invitee_location'
-    t.boolean  'delivered',        default: false
-    t.boolean  'registered',       default: false
-    t.datetime 'created_at',                       null: false
-    t.datetime 'updated_at',                       null: false
-    t.index ['user_id'], name: 'index_invitations_on_user_id', using: :btree
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table 'profiles', force: :cascade do |t|
-    t.integer  'user_id'
-    t.string   'location'
-    t.boolean  'receive_emails',     default: false
-    t.boolean  'receive_job_alerts', default: false
-    t.text     'biography'
-    t.datetime 'created_at',                         null: false
-    t.datetime 'updated_at',                         null: false
-    t.index ['user_id'], name: 'index_profiles_on_user_id', using: :btree
+  create_table "invitations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "invitee_name"
+    t.string   "invitee_email"
+    t.string   "invitee_title"
+    t.string   "invitee_company"
+    t.string   "invitee_location"
+    t.boolean  "delivered",        default: false
+    t.boolean  "registered",       default: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.index ["user_id"], name: "index_invitations_on_user_id", using: :btree
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'email',                  default: '', null: false
-    t.string   'encrypted_password',     default: '', null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count',          default: 0,  null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.inet     'current_sign_in_ip'
-    t.inet     'last_sign_in_ip'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.string   'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string   'unconfirmed_email'
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true, using: :btree
-    t.index ['email'], name: 'index_users_on_email', unique: true, using: :btree
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "location"
+    t.boolean  "receive_emails",     default: false
+    t.boolean  "receive_job_alerts", default: false
+    t.text     "biography"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
-  add_foreign_key 'invitations', 'users'
-  add_foreign_key 'profiles', 'users'
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  add_foreign_key "invitations", "users"
+  add_foreign_key "profiles", "users"
 end
