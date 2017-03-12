4b5f2d2a2829409b04b04a74af8c49f628ade844
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2017-03-12 18:37:15 +0200

    Comment directory link. Add public IP address of menadevs.com. Modify text on frontpage

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :branch, ENV.fetch("CAPISTRANO_BRANCH", "master")
 set :mb_sidekiq_concurrency, 1
 
-server "mena-devs.com",
+server "138.68.149.87",
        :user => "deployer",
        :roles => %w(app backup cron db redis sidekiq web)
