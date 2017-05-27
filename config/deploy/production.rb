b1402a1cd0f37920100844d7a3fae83b20440cd9
Constantine Nicolaou <constantine.nicolaou@keeward.com>
2017-05-27 22:26:24 +0300

    Add RoR pragprog book for reference

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-set :branch, ENV.fetch("CAPISTRANO_BRANCH", "master")
-# set :mb_sidekiq_concurrency, 1
-
-server "138.68.161.30",
-       :user => 'root',
-       :roles => %w(app cron db web)
-      #  :roles => %w(app backup cron db redis sidekiq web)
-# BlueM00n
