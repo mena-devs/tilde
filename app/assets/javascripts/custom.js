adebefb53429ad9b7f5221cea4744024e643121a
Hassan Kanj <hassan@qual.tech>
2017-06-16 12:11:38 +0300

    Remove extra spaces

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,19 +1,14 @@
 // custom javascript code
-
 $("document").ready(function() {
    // show confirmation box (in case the user clicked it by mistake instead of clicking 'save')
-   $(".back-btn").on("click", function() {
-    if(!confirm("Go back?")) {
-        return false;
-    }
-   })
-
-
-   $(".skills-input").select2({
-    theme: "bootstrap",
-    tags: true,
-    tokenSeparators: [',']
-});
-
-
+    $(".back-btn").on("click", function() {
+      if(!confirm("Go back?")) {
+          return false;
+      }
+    }); // back-btn
+    $(".skills-input").select2({
+      theme: "bootstrap",
+      tags: true,
+      tokenSeparators: [',']
+    }); // select2
 });
