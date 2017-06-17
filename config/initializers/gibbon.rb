66cfe8c25731c9832375638ffacb4d7e41f60132
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2017-06-17 16:38:00 +0300

    Initializers in development environment
    When using the application in development environment, avoid loading production services and their configuration files

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,2 +1,4 @@
-Gibbon::Request.api_key = AppSettings.mailchimp_api_key
-Gibbon::Request.timeout = 15
+unless Rails.env.development?
+  Gibbon::Request.api_key = AppSettings.mailchimp_api_key
+  Gibbon::Request.timeout = 15
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :slack,
-           AppSettings.slack_app_client_id,
-           AppSettings.slack_app_client_secret,
-           scope: 'users:read users:read.email'
+if !AppSettings.slack_app_client_id.blank? && !AppSettings.slack_app_client_secret.blank?
+  Rails.application.config.middleware.use(OmniAuth::Builder) do
+    provider :slack,
+             AppSettings.slack_app_client_id,
+             AppSettings.slack_app_client_secret,
+             scope: 'users:read users:read.email'
+  end
+else
+  raise Exception.new("Please configure /config/settings/*.yml with correct values")
 end
