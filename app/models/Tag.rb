29bcd294d1f00f618f4a6d993714d174c12bf09d
Hassan Kanj <hassan@qual.tech>
2017-06-17 12:56:49 +0300

    Add schema information to Tag

--- app/models/Tag.rb
+++ app/models/Tag.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id             :integer          not null, primary key
+#  is_approved    :boolean          default(FALSE)
+#  name           :string
+#  taggings_count :integer          default(0)
+#
+# Indexes
+#
+#  index_tags_on_name  (name) UNIQUE
+#
+
 # Table name: tags
 class Tag < ApplicationRecord
 end
