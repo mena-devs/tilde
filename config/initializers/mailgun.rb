0162b783deb6f798bb661c0fdba13a8ffd0da9a5
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2017-06-17 16:38:00 +0300

    Initializers in development environment
    When using the application in development environment, avoid loading production services and their configuration files

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,3 +1,5 @@
-Mailgun.configure do |config|
-  config.api_key = AppSettings.mailgun_api_key
+unless Rails.env.development?
+  Mailgun.configure do |config|
+    config.api_key = AppSettings.mailgun_api_key
+  end
 end
