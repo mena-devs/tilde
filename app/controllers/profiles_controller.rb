81d237771e3a7d13855f1d3f8aff487fd00ad54f
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2017-06-24 18:41:47 +0300

    Updates to profile layout and links

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update]
+  before_action :authenticate_user!
 
   # GET /profiles/1
   def show
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,17 @@
 module ProfilesHelper
+  def name(profile)
+    profile.user.name.titleize
+  end
+
+  def email(profile)
+    profile.user.email
+  end
+
+  def nickname(profile)
+    if profile.nickname.blank?
+      "<strong>not-set</strong>".html_safe
+    else
+      profile.nickname.downcase
+    end
+  end
 end
