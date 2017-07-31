8712689291d48da47e41e7c9693bc3b967544a6b
Constantine Nicolaou <constantine.nicolaou@keeward.com>
2017-07-31 00:30:37 -0400

    Job details display formatting

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,9 +1,19 @@
 module JobsHelper
-  def job_invitee_name(job)
+  def job_creator_name(job)
     if job.user
       job.user.try(:name)
     else
       "Not defined"
     end
+  end
+
+  def job_salary(job)
+  	if job.to_salary.blank?
+  		job_salary = "starting #{job.from_salary}"
+  	else
+  		job_salary = "between #{job.from_salary} and #{job.to_salary}"
+  	end
+
+  	job_salary
   end
 end
