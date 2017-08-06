95b175f6e529bb5598c9499e1a4f9f116422ff90
Constantine Nicolaou <constantine.nicolaou@keeward.com>
2017-08-05 23:17:40 -0400

    Change order of jQuery and jQuery_ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require plugins
 //= require functions
 //= require tinymce
+//= require jquery
+//= require jquery_ujs
