a8c3155a85380dcfacaa2889fb90c926f347befa
Constantine Nicolaou <constantine.nicolaou@keeward.com>
2017-10-24 17:02:04 +0300

    Display additional information for users trying to login using Slack credentials when they're not members

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def slack
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
+    if @user && @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
     else
       # TODO: log errors generated from authenticating from omniauth
@@ -10,7 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       logger.info("An error has occured")
       logger.info(request.env["omniauth.auth"])
 
-      redirect_to new_user_registration_url, alert: 'An error has occured while signing in using your Slack identity. Please try again in a few minutes.'
+      redirect_to new_user_registration_url, alert: 'An error has occured while signing in using your Slack identity. If you are not a member of MENAdevs Slack group, you need to signup using your email address before logging in.'
     end
   end
 
