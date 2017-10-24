642e1b201c72b9da8ad9f34682161fa4b289ade3
Constantine Nicolaou <constantine.nicolaou@keeward.com>
2017-10-24 16:37:45 +0300

    Fix another typo with validating salary range

--- app/validators/salary_validator.rb
+++ app/validators/salary_validator.rb
@@ -1,6 +1,6 @@
 class SalaryValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors[attribute] << (options[:message] || "cannot be less than starting salary") unless valid_salary_fields?(record)
+    record.errors[attribute] << (options[:message] || "cannot be less than starting salary") if valid_salary_fields?(record)
   end
 
   def valid_salary_fields?(record)
