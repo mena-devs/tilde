c74b36acd12631e9bbd0a6ddfa87855ad4e46988
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2017-12-19 15:12:33 +0200

    Missing decorator on job_mailer job object

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -2,7 +2,7 @@ class JobMailer < ApplicationMailer
   add_template_helper(JobsHelper)
 
   def new_job(job_id)
-    @job = Job.find(job_id)
+    @job = Job.find(job_id).decorate
 
     subject = "MENAdevs - #{@job.user.try(:name)} posted a new job"
 
@@ -11,7 +11,7 @@ class JobMailer < ApplicationMailer
   end
 
   def job_published(job_id)
-    @job = Job.find(job_id)
+    @job = Job.find(job_id).decorate
 
     subject = "MENAdevs - Your job is now posted on the job board"
 
@@ -20,7 +20,7 @@ class JobMailer < ApplicationMailer
   end
 
   def job_unpublished(job_id)
-    @job = Job.find(job_id)
+    @job = Job.find(job_id).decorate
 
     subject = "MENAdevs - Your job post is now offline"
 
