c7393f84dc1d61c5cfee4aea22d7a48b4bf09bf7
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2018-03-06 01:02:48 +0200

    ENV has REDIS_URL not redis_url, case matters

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
-redis_url = ENV.fetch("redis_url")
+redis_url = ENV.fetch("REDIS_URL")
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_url}:6379/0", :namespace => "mena_devs_com" }
+  config.redis = { url: "redis://#{redis_url}:6379/12", :namespace => "mena_devs_com" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_url}:6379/0", :namespace => "mena_devs_com" }
+  config.redis = { url: "redis://#{redis_url}:6379/12", :namespace => "mena_devs_com" }
 end
 
 require "sidekiq/web"
