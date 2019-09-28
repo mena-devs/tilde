3d65bc9c9d77e0ed42d7f6071368bfb89ce99229
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2019-09-28 19:30:44 +0200

    Add coverage library to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'simplecov'
+SimpleCov.start
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
