782a1cd84acba5c68c50062a8c5de05c0cf0f544
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2019-12-26 15:50:29 +0200

    Load Slack notifier configuration only in production and development envs

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,3 +1,5 @@
-@notifier = Slack::Notifier.new(AppSettings.slack_web_hook_url,
-                                channel: '#default',
-                                username: 'Tilde')
+if Rails.env.production? || Rails.env.development?
+  @notifier = Slack::Notifier.new(AppSettings.slack_web_hook_url,
+                                  channel: '#default',
+                                  username: 'Tilde')
+end
