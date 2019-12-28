dc37286fc5c4181d7f8961f50788b7df37cf6d3e
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2019-12-29 00:13:20 +0200

    Remove .simplecov file as it is unnecessary at this stage

--- .simplecov
+++
@@ -1,8 +0,0 @@
-require "simplecov"
-SimpleCov.start("rails") do
-  add_filter("/bin/")
-  add_filter("/lib/tasks/auto_annotate_models.rake")
-  add_filter("/lib/tasks/coverage.rake")
-end
-# TODO: Change line below to 90
-SimpleCov.minimum_coverage(50)
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,8 @@
+ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
 SimpleCov.start
 
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
