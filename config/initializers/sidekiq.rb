cc5cf2442e4c5e34a6aeae0a21f6b0cc067dc361
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2020-01-29 22:01:33 +0100

    Remove basic auth for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,9 @@
 redis_url = ENV.fetch("REDIS_URL")
+
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{redis_url}:6379/12", :namespace => "mena_devs_com" }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: "redis://#{redis_url}:6379/12", :namespace => "mena_devs_com" }
-end
-
-require "sidekiq/web"
-Sidekiq::Web.app_url = "/"
-Sidekiq::Web.use(Rack::Auth::Basic, "Application") do |username, password|
-  username == ENV.fetch("SIDEKIQ_WEB_USERNAME") &&
-  password == ENV.fetch("SIDEKIQ_WEB_PASSWORD")
 end
