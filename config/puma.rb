0c9f4b1957882ccad8266bf423c0586230efcb4a
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2020-03-21 18:40:36 +0100

    Update puma.rb with app details

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,23 @@
-rails_env = "production"
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+
+# Default to production
+rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
-app_dir = "/var/www/menadevs"
+port ENV.fetch("PORT") { 3000 }
 
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
+
+# Change to match your CPU core count
+workers 1
+
+# Min and Max threads per worker
+threads 1, 6
+
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
 bind  "unix://#{app_dir}/puma.sock"
 pidfile "#{app_dir}/puma.pid"
 state_path "#{app_dir}/puma.state"
@@ -10,11 +25,12 @@ directory "#{app_dir}/"
 
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
-workers 2
-threads 1,2
-
-daemonize true
-
 activate_control_app "unix://#{app_dir}/pumactl.sock"
 
 prune_bundler
+
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
