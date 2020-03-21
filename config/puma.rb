2ad53c3d1197731fa8f3f4ec10f92e3e42b5169f
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2020-03-21 18:05:17 +0100

    Update location of puma socket file

--- config/puma.rb
+++ config/puma.rb
@@ -65,7 +65,7 @@ rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+bind "unix:#{app_dir}/tmp/sockets/puma.sock"
 
 activate_control_app
 
