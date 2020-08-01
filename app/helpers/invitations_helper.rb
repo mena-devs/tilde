c780a250621d8e7cb3305f5ab5f56c8ff37c9a79
Constantine Nikolaou <constantin.nicolaou@gmail.com>
2020-08-01 18:20:27 +0200

    Improve invitations view for member
    Allow member to filter through their invitations
    
    Enable detailed invitation view for members

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -1,7 +1,30 @@
 module InvitationsHelper
   def display_invitation_state(invitation)
-    if invitation.aasm_state != 'accepted'
-      'pending'
+    (invitation.aasm_state != 'accepted') ? 'Pending' : invitation.aasm_state.titleize
+  end
+
+  def display_tilde_membership(invitation)
+    invitation.registered? ? 'Yes' : 'No'
+  end
+
+  def display_tilde_member(invitation)
+    if invitation.registered?
+      user = User.find_by_email(invitation.invitee_email)
+      link_to(invitation.invitee_email, directory_user_path(user.custom_identifier))
+    else
+      invitation.invitee_email
     end
+  end
+
+  def display_button_filter_class(parameters, state=nil)
+    class_data = 'button button-directory button-3d button-mini button-rounded'
+    
+    if (state.nil? && !parameters.has_key?(:state))
+      class_data += ' button-teal'
+    elsif (!state.nil? && parameters[:state] == state)
+      class_data += ' button-teal'
+    end
+
+    class_data
   end
 end
