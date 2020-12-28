c44e3edba44e73ca8ed3b0baed14bb7d0b61d6e7
Constantine Nikolaou <constantin.nicolaou@gmail.com>
2020-12-28 16:32:51 +0000

    Delete test/mailers folder

--- test/mailers/previews/notifications_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/notifications_mailer
-class NotificationsMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/notifications_mailer/announcement
-  def announcement
-    NotificationsMailer.announcement
-  end
-
-end
