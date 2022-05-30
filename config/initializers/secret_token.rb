b10c4c3962e4b2cf5035a7ec729e06dcea81bed2
Constantine Nikolaou <constantin.nicolaou@gmail.com>
2022-05-30 22:15:57 +0200

    Update secret_token.rb to use placeholder when running tests

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_key_base = ENV.fetch("SECRET_KEY_BASE")
+if Rails.env.test?
+    Rails.application.config.secret_key_base = '9489b3eee4eccf317ed77407553e8adc97baca7c74dc7ee33cd93e4c8b69477eea66eaedeb18af0be2679887c7c69c0a28c0fded0a71ea472a8c4laalal19cb'
+else
+    Rails.application.config.secret_key_base = ENV.fetch("SECRET_KEY_BASE")
+end
