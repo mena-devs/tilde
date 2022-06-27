539a972d571fce756dcc4679c7406fb85965f872
Constantine Nikolaou <constantin.nicolaou@gmail.com>
2022-06-27 15:30:53 +0200

    Further refactoring

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,8 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless (email_valid?(value) && handle_contains_number_of_dots(value) && handle_last_character_is_not_dot(value))
-      record.errors[attribute] << email_invalid_error_message
-    end
+    record.errors[attribute] << email_invalid_error_message unless email_valid?(value) && handle_contains_number_of_dots(value) && handle_last_character_is_not_dot(value)
   end
 
   private
